c2a96464b36941e67fc8faa735e8a2c1d6174f84
windlively <windlively@live.cn>
2021-04-22 00:01:17 +0800

    add: DecodeWays

---
+++ tools/src/main/java/lucky/baijunhan/tools/T2.java
@@ -0,0 +1,29 @@
+package lucky.baijunhan.tools;
+
+public class T2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(pattern("abcccedeeff", "deef"));
+    }
+
+
+    public static String pattern(String N, String M){
+        char[] sN = N.toCharArray();
+        char[] sM = M.toCharArray();
+
+        for (int i = 0; i < sN.length; i++) {
+            int k = i;
+            int j = 0;
+            for (j = 0; j < sM.length; j++) {
+                if(sN[k ++] != sM[j]) break;
+            }
+            if(j == sM.length){
+                return M;
+            }
+        }
+
+        return "";
+
+    }
+}
